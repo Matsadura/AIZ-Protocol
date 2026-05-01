059e9e52304e480d60420b345315fb430eb66aea
Matsadura <zidanditoo1@gmail.com>
2026-05-01 21:38:18 +0100

    [UPDATE] folder structure

--- src/request/request.cpp
+++ src/request/request.cpp
@@ -3,7 +3,7 @@
 /**
  * Constructor for the Request class.
  */
-Request::Request( void ) :
+Request::Request(void) :
     m_state(REQUEST_LINE),
     m_error_code(0),
     m_max_body_size(1024 * 1024),
@@ -18,7 +18,7 @@ Request::Request( void ) :
 /**
  * Copy constructor for the Request class.
  */
-Request::Request(const Request& other) :
+Request::Request(const Request &other) :
     m_state(other.m_state),
     m_raw_buffer(other.m_raw_buffer),
     m_error_code(other.m_error_code),
@@ -41,26 +41,26 @@ Request::Request(const Request& other) :
 /**
  * Assignment operator for the Request class.
  */
-Request& Request::operator=(const Request& other)
+Request &Request::operator=(const Request &other)
 {
     if (this != &other)
     {
-        m_state = other.m_state;
-        m_raw_buffer = other.m_raw_buffer;
-        m_error_code = other.m_error_code;
-        m_max_body_size = other.m_max_body_size;
-        m_method = other.m_method;
-        m_uri = other.m_uri;
-        m_path = other.m_path;
-        m_query = other.m_query;
-        m_version = other.m_version;
-        m_headers = other.m_headers;
-        m_body = other.m_body;
-        m_is_chunked = other.m_is_chunked;
-        m_content_length = other.m_content_length;
-        m_chunk_state = other.m_chunk_state;
+        m_state              = other.m_state;
+        m_raw_buffer         = other.m_raw_buffer;
+        m_error_code         = other.m_error_code;
+        m_max_body_size      = other.m_max_body_size;
+        m_method             = other.m_method;
+        m_uri                = other.m_uri;
+        m_path               = other.m_path;
+        m_query              = other.m_query;
+        m_version            = other.m_version;
+        m_headers            = other.m_headers;
+        m_body               = other.m_body;
+        m_is_chunked         = other.m_is_chunked;
+        m_content_length     = other.m_content_length;
+        m_chunk_state        = other.m_chunk_state;
         m_current_chunk_size = other.m_current_chunk_size;
-        m_chunk_bytes_read = other.m_chunk_bytes_read;
+        m_chunk_bytes_read   = other.m_chunk_bytes_read;
     }
     return *this;
 }
@@ -68,6 +68,6 @@ Request& Request::operator=(const Request& other)
 /**
  * Destructor for the Request class.
  */
-Request::~Request( void )
+Request::~Request(void)
 {
 }
---
+++ src/request/request.hpp
@@ -0,0 +1,78 @@
+#ifndef REQUEST_HPP
+#define REQUEST_HPP
+
+#include <cstddef>
+#include <map>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+class Request
+{
+  public:
+    enum ParserState
+    {
+        REQUEST_LINE,
+        HEADERS,
+        BODY,
+        COMPLETE,
+        ERROR
+    };
+
+    enum ChunkState
+    {
+        CHUNK_SIZE,
+        CHUNK_DATA,
+        CHUNK_TRAILER
+    };
+
+    Request(void);
+    Request(const Request &other);
+    Request &operator=(const Request &other);
+    ~Request(void);
+
+    void appendData(const char *data, size_t length);
+    void setMaxBodySize(size_t max_size);
+
+    ParserState getState() const;
+    int getErrorCode() const;
+    bool isComplete() const;
+
+    std::string getMethod() const;
+    std::string getURI() const;
+    std::string getVersion() const;
+    std::string getHeader(const std::string &key) const;
+    std::map<std::string, std::string> getHeaders() const;
+    const std::vector<char> &getBody() const;
+
+  private:
+    ParserState m_state;
+    std::string m_raw_buffer;
+    int m_error_code;
+    size_t m_max_body_size;
+
+    std::string m_method;
+    std::string m_uri;
+    std::string m_path;
+    std::string m_query;
+    std::string m_version;
+    std::map<std::string, std::string> m_headers;
+    std::vector<char> m_body;
+
+    bool m_is_chunked;
+    size_t m_content_length;
+
+    ChunkState m_chunk_state;
+    size_t m_current_chunk_size;
+    size_t m_chunk_bytes_read;
+
+    void parseRequestLine();
+    void parseHeaders();
+    void parseBody();
+    void parseChunkedBody();
+
+    std::string decodeURI(const std::string &uri);
+    void exctractPathAndQuery();
+};
+
+#endif /* REQUEST_HPP */
