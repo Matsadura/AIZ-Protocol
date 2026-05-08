0914bddf17062bdc3a1336b5023e42782042a3c4
ila36ix <jbariali002@gmail.com>
2026-05-08 02:31:30 +0100

    [UPDATE] format errors

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
-    std ::cout << "Whenever you compile, try to provide the path of the file where you're current main is implemented!\n\n"
-               << "     $ make re MAIN_PATH=tests/test_request/test_headers.cpp\n\n";
+    std ::cout
+        << "Whenever you compile, try to provide the path of the file where you're current main is implemented!\n\n"
+        << "     $ make re MAIN_PATH=tests/test_request/test_headers.cpp\n\n";
 }
