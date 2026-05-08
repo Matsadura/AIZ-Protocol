46924e4e0c858085d307051e3832b321ce2b5ba1
ila36ix <jbariali002@gmail.com>
2026-05-08 09:54:24 +0100

    [ADD] multiple ports listeners container

---
+++ src/core/Listeners.cpp
@@ -0,0 +1,61 @@
+#include "Listeners.h"
+
+int Listeners::PandingLimit = 1024;
+
+Listeners::Listeners()
+{
+}
+
+Listeners::Listeners(const Listeners &other) // NOLINT
+{
+}
+
+Listeners &Listeners::operator=(const Listeners &other) // NOLINT
+{
+    return *this;
+}
+
+Listeners::~Listeners()
+{
+    std::for_each(m_sockFds.begin(), m_sockFds.end(), close);
+    m_sockFds.clear();
+}
+
+void Listeners::create_new(const char *nodeName, const char *port)
+{
+    ListenerAddrInfo ai(nodeName, port);
+    int sockfd;
+
+    sockfd  = socket(ai.family(), ai.sockType(), 0);
+    int yes = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+    {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    if (bind(sockfd, ai.addr(), ai.addr_len()) != 0)
+        abort("bind");
+
+    if (listen(sockfd, Listeners::PandingLimit) != 0)
+        abort("lister");
+
+    std::cout << "INFO: Listening at " << ai.toString() << "\n";
+    std::cout << "INFO: " << sockfd << " file descriptor of listener socket\n";
+    m_sockFds.push_back(sockfd);
+}
+
+bool Listeners::contains(int fd)
+{
+    return std::find(m_sockFds.begin(), m_sockFds.end(), fd) != m_sockFds.end();
+}
+
+std::size_t Listeners::size()
+{
+    return m_sockFds.size();
+}
+
+int Listeners::operator[](std::size_t index)
+{
+    return m_sockFds.at(index);
+}
