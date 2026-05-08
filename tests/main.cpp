54ee9a4be132f4ba3b87cfb83da0231815355730
ila36ix <jbariali002@gmail.com>
2026-05-08 02:34:02 +0100

    [UPDATE] typo

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,7 +2,6 @@
 
 int main()
 {
-    std ::cout
-        << "Whenever you compile, try to provide the path of the file where you're current main is implemented!\n\n"
-        << "     $ make re MAIN_PATH=tests/test_request/test_headers.cpp\n\n";
+    std::cout << "Whenever you compile, try to provide the path of the file where your current main is implemented!\n\n"
+              << "     $ make re MAIN_PATH=tests/test_request/test_headers.cpp\n\n";
 }
