2cbd5d047aba17c286e4f3df0cf72485edbbf9cc
ImaneJb1 <imaane@gmail.com>
2026-06-13 16:57:33 +0100

    [ADD] init response

--- src/Response/Response.cpp
+++ src/Response/Response.cpp
@@ -1,11 +1,72 @@
 #include "Response.hpp"
 
 Response::Response(const Request &request) :
-    m_state(RESPONSE_INIT), m_status_code(request.getErrorCode()), m_request(request)
+    m_state(RESPONSE_INIT),
+    m_buffer_offset(0),
+    m_status_code(request.getErrorCode()), 
+    m_request(request)
 
 {
 }
 
 Response::~Response(void)
 {
+}
+
+/**
+ * validate the request and prepare the response
+ */
+
+void   Response::init_response()
+{
+    if(m_request.getState() != Request::COMPLETE)
+    {
+        m_state = ERROR;
+        m_status_code = m_request.getErrorCode();
+        return;
+    }
+    else
+    {
+        struct stat bff = {};
+        std::string file = "index.html"; // index.html to be replaced by the actual path provided by the router
+        if (stat(file.c_str(), &bff) != 0) 
+        {
+            m_state = ERROR;
+            if(errno == ENOENT)
+                m_status_code = NOT_FOUND;
+            else if(errno == EACCES)
+                m_status_code = FORBIDDEN;
+            return;
+        }
+        m_file_input.open(file.c_str());
+        if(!m_file_input)
+        {
+            m_status_code = INTERNAL_SERVER_ERROR;
+            m_state = ERROR;
+            return;
+        }
+        m_state = RESPONSE_SEND_HEADERS;
+    }
+}
+
+void Response::process(int fd)
+{
+    switch (m_state) 
+    {
+        case RESPONSE_INIT:
+            //init response*fd
+            break;
+        case RESPONSE_SEND_HEADERS:
+            // sending chunks
+            break;
+        case RESPONSE_SEND_CHUNKS:
+            // sending chunks
+            break;
+        case ERROR:
+            //handle error
+            break;
+        case RESPONSE_COMPLETE:
+            ///
+            break;
+    }
 }
--- src/Response/Response.hpp
+++ src/Response/Response.hpp
@@ -2,24 +2,37 @@
 
 #include "../Request/Request.hpp"
 #include <fstream>
+#include <cerrno>
+#include <sys/stat.h>
 
 #define NOT_FOUND 404
-
+#define OK 200
+#define FORBIDDEN 403
+#define INTERNAL_SERVER_ERROR 500
 class Response
 {
-    public:
-        enum ResponseState
-        {
-            RESPONSE_INIT,
-            RESPONSE_SEND_HEADERS,
-            RESPONSE_SEND_CHUNKS,
-            RESPONSE_COMPLETE
-        };
-        Response(const Request &request);
-        ~Response(void);
-    private:
-        ResponseState m_state;
-        int         m_status_code;
-        std::ifstream m_file_input;
-        Request m_request;
+  public:
+    enum ResponseState
+    {
+        RESPONSE_INIT,
+        RESPONSE_SEND_HEADERS,
+        RESPONSE_SEND_CHUNKS,
+        RESPONSE_COMPLETE,
+        ERROR
+    };
+
+    Response(const Request &request);
+    ~Response(void);
+
+    void process(int fd);
+
+  private:
+    ResponseState m_state;
+    size_t m_buffer_offset;
+    int m_status_code;
+    std::ifstream m_file_input;
+    Request m_request;
+
+    void   init_response();
+
 };
