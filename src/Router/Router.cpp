0d296b237c1b84b13c44eb649a400ec5fde17e2d
ila36ix <jbariali002@gmail.com>
2026-07-15 06:43:42 +0100

    [UPDATE] Add RouterResource to use in all methods

--- src/Router/Router.cpp
+++ src/Router/Router.cpp
@@ -1,331 +1,97 @@
 #include "Router.hpp"
 
-/**
- * Tells if @resource requested by the user matches the @location_path at the config file
- * so "/img" matches "/img" and "/img/x.png", but NOT "/images/x.png"
- *
- * Return: true meaning @loc_path is a possible match for @resource, or false meaning the opposite in case you still
- * reading scream "THIS IS OUR REVIVALE"
- */
-bool router_location_matcher(const std::string &resource, const std::string &loc_path)
+Router::Router(const s_Server &server, const std::string &uri, const std::string &method) :
+    m_server(server), m_uri(uri), m_method(method), m_resource(server, uri, method)
 {
-    if (!string_start_with(resource, loc_path))
-        return false;
-    if (resource.size() == loc_path.size())
-        return true;
-    if (loc_path[loc_path.size() - 1] == '/')
-        return true;
-    return resource[loc_path.size()] == '/';
 }
 
-/**
- * Get the best matching config file location for the @resource
- *
- * Return: pointer for the config file location matching the resource or NULL
- */
-const s_Location *router_get_best_matched_location(const s_Server &server, const std::string &resource)
+RouterResult Router::get_result()
 {
-    const s_Location *location = NULL;
-
-    for (size_t j = 0; j < server.locations.size(); ++j)
+    if (m_resource.has_early_response())
     {
-        if (router_location_matcher(resource, server.locations[j].path))
+        return m_resource.get_early_router_result();
+    }
+    if (m_method == "GET")
+    {
+        return handle_get();
+    }
+    return forbidden();
+}
+
+RouterResult Router::init_error_result(int http_code)
+{
+    const std::string &file_path = RouterResource::get_default_page(m_server, http_code);
+    return RouterResult(http_code, file_path, RouterResult::FILE_PATH);
+}
+
+RouterResult Router::not_found()
+{
+    return init_error_result(404);
+}
+
+RouterResult Router::forbidden()
+{
+    return init_error_result(403);
+}
+
+RouterResult Router::directory_listing(const std::string &dir_path)
+{
+    return RouterResult(200, generate_directory_listing(dir_path, m_uri), RouterResult::STRING_BUFFER);
+}
+
+RouterResult Router::redirection(int http_code, const std::string &rediretion_location)
+{
+    return RouterResult(http_code, rediretion_location, RouterResult::REDIRECTION);
+}
+
+RouterResult Router::handle_get()
+{
+    std::string disk_path = m_resource.get_disk_path();
+
+    if (!m_resource.exists())
+    {
+        router_log_helper(m_uri, m_resource.get_location(), disk_path, "File or directory does not exist", 404);
+        return not_found();
+    }
+
+    if (m_resource.is_directory())
+    {
+        if (m_uri.empty() || m_uri[m_uri.size() - 1] != '/')
         {
-            if (!location || server.locations[j].path.size() > location->path.size())
+            router_log_helper(m_uri, m_resource.get_location(), m_uri + "/",
+                              "Directory; redirecting with trailing slash", 301);
+            return redirection(301, m_uri + "/");
+        }
+
+        const s_Location *loc = m_resource.get_location();
+
+        std::string indexPath = RouterResource::join_path(disk_path, loc->index);
+        if (!loc->index.empty() && is_file_regular(indexPath))
+        {
+            disk_path = indexPath;
+        }
+        else if (loc->autoindex)
+        {
+            if (!is_file_readable(disk_path))
             {
-                location = &server.locations[j];
+                router_log_helper(m_uri, loc, disk_path, "Directory is unreadable for listing", 403);
+                return forbidden();
             }
-        }
-    }
-    return location;
-}
-
-/**
- * Simple logging utility for debugging, sometime in the near future you'll ask what WTF that just happend this will
- * help!
- */
-void router_log_helper(const std::string &resource, const s_Location *loc, const std::string &file_path,
-                       const std::string &msg, int http_code)
-{
-    LOG_DEBUG("ROUTER") << "[URI=" << resource << "]";
-    if (loc)
-        std::cout << " [config_loc=" << loc->path << "]";
-    std::cout << " [";
-    if (!file_path.empty())
-        std::cout << file_path;
-    std::cout << ": " << msg << "]" << " (status_code=" << http_code << ")\n";
-}
-
-bool is_file_regular(const std::string &path)
-{
-    struct stat file_info = {};
-
-    return stat(path.c_str(), &file_info) == 0 && S_ISREG(file_info.st_mode);
-}
-
-bool is_file_executable(const std::string &filepath)
-{
-    return access(filepath.c_str(), X_OK) == 0;
-}
-
-/**
- * Return: path of the default page for the @status_code or empty string meaning use your default, you don't have a
- * default? that you problem!
- *
- * NOTE: Each server could have muliple default paths foreach error page, this will get them for you
- */
-std::string get_default_page(const s_Server &server, int status_code)
-{
-    std::map<int, std::string>::const_iterator it = server.error_page.find(status_code);
-    if (it == server.error_page.end())
-        return "";
-    std::string page_path = it->second;
-    if (is_file_regular(page_path))
-    {
-        return page_path;
-    }
-    return "";
-}
-
-bool is_file_readable(const std::string &filepath)
-{
-    return access(filepath.c_str(), R_OK) == 0;
-}
-
-struct CgiMetaData
-{
-    bool        is_cgi;
-    std::string script_path;
-    std::string path_info;
-    std::string interpreter_path;
-
-    CgiMetaData() : is_cgi(false)
-    {
-    }
-
-    CgiMetaData(bool is_cgi_val, const std::string &script, const std::string &info, const std::string &interpreter) :
-        is_cgi(is_cgi_val), script_path(script), path_info(info), interpreter_path(interpreter)
-    {
-    }
-};
-
-CgiMetaData router_request_is_cgi(const s_Server &server, const Request &req)
-{
-    const s_Location *location = router_get_best_matched_location(server, req.getPath());
-
-    if (!location || location->CGIhandlers.empty() || location->root.empty())
-    {
-        return CgiMetaData();
-    }
-
-    std::string sys_path = join_path(location->root, req.getPath().substr(location->path.size()));
-    std::string script_path;
-    std::string path_info;
-
-    for (std::size_t i = 0; i <= sys_path.size(); i++)
-    {
-        if (i == sys_path.size() || sys_path[i] == '/')
-        {
-            std::string current_path = sys_path.substr(0, i);
-            // TODO: Not save! location->root will be counted going above root will happen anyway
-            if (!path_traverse_is_safe(current_path))
-            {
-                return CgiMetaData();
-            }
-            if (is_file_regular(current_path))
-            {
-                script_path = current_path;
-                if (i < sys_path.size())
-                {
-                    path_info = sys_path.substr(i);
-                }
-                break;
-            }
-        }
-    }
-
-    if (script_path.empty())
-    {
-        return CgiMetaData();
-    }
-
-    std::size_t dot_pos   = script_path.find_last_of('.');
-    std::size_t slash_pos = script_path.find_last_of('/');
-
-    if (dot_pos != std::string::npos && (slash_pos == std::string::npos || dot_pos > slash_pos))
-    {
-        std::string                                        ext = script_path.substr(dot_pos);
-        std::map<std::string, std::string>::const_iterator it  = location->CGIhandlers.find(ext);
-
-        if (it != location->CGIhandlers.end())
-        {
-            return CgiMetaData(true, script_path, path_info, it->second);
-        }
-    }
-
-    return CgiMetaData();
-}
-
-RouterResult router_get_resource(const s_Server &server, const std::string &resource)
-{
-    const s_Location *location  = router_get_best_matched_location(server, resource);
-    struct stat       file_info = {};
-
-    // TODO: Handle request type GET/HEAD...
-
-    if (!location)
-    {
-        router_log_helper(resource, location, "", "no matching location", 404);
-        return RouterResult(404, get_default_page(server, 404), RouterResult::FILE_PATH);
-    }
-
-    if (location->redirect_code != 0)
-    {
-        std::string redirect_uri(resource);
-        redirect_uri.erase(0, location->path.size());
-        redirect_uri = join_path(location->redirect_path, redirect_uri);
-        if (redirect_uri[0] != '/')
-        {
-            redirect_uri = "/" + redirect_uri;
-        }
-        router_log_helper(resource, location, redirect_uri, "Redirection location", location->redirect_code);
-        return RouterResult(location->redirect_code, redirect_uri, RouterResult::REDIRECTION);
-    }
-
-    if (location->root.empty())
-    {
-        router_log_helper(resource, location, "", "Root directory is not configured", 404);
-        return RouterResult(404, get_default_page(server, 404), RouterResult::FILE_PATH);
-    }
-
-    std::string sys_path(resource);
-    sys_path.erase(0, location->path.size());
-
-    if (!path_traverse_is_safe(sys_path)) // Prevent path traversing like: ../././../..
-    {
-        router_log_helper(resource, location, sys_path, "Attempting to go above root directory", 400);
-        return RouterResult(400, get_default_page(server, 400), RouterResult::FILE_PATH);
-    }
-
-    sys_path = join_path(location->root, sys_path);
-
-    if (stat(sys_path.c_str(), &file_info) != 0)
-    {
-        router_log_helper(resource, location, sys_path, "File or directory does not exist", 404);
-        return RouterResult(404, get_default_page(server, 404), RouterResult::FILE_PATH);
-    }
-    if (S_ISDIR(file_info.st_mode))
-    {
-        if (resource.empty() || resource[resource.size() - 1] != '/')
-        {
-            router_log_helper(resource, location, resource + "/", "Directory; redirecting with trailing slash", 301);
-            return RouterResult(301, resource + "/", RouterResult::REDIRECTION);
-        }
-
-        std::string indexPath = join_path(sys_path, location->index);
-        if (!location->index.empty() && is_file_regular(indexPath))
-        {
-            sys_path = indexPath;
-        }
-        else if (location->autoindex)
-        {
-            if (!is_file_readable(sys_path))
-            {
-                router_log_helper(resource, location, sys_path, "Directory is unreadable for listing", 403);
-                return RouterResult(403, get_default_page(server, 403), RouterResult::FILE_PATH);
-            }
-            router_log_helper(resource, location, sys_path, "Directory listing will be performed", 200);
-            return RouterResult(200, generate_directory_listing(sys_path, resource), RouterResult::STRING_BUFFER);
+            router_log_helper(m_uri, loc, disk_path, "Directory listing will be performed", 200);
+            return directory_listing(disk_path);
         }
         else
         {
-            router_log_helper(resource, location, sys_path, "Directory with no index file and listing is off", 403);
-            return RouterResult(403, get_default_page(server, 403), RouterResult::FILE_PATH);
+            router_log_helper(m_uri, loc, disk_path, "Directory with no index file and listing is off", 403);
+            return forbidden();
         }
     }
 
-    if (!is_file_readable(sys_path))
+    if (!is_file_readable(disk_path))
     {
-        router_log_helper(resource, location, sys_path, "File permission denied", 403);
-        return RouterResult(403, get_default_page(server, 403), RouterResult::FILE_PATH);
+        router_log_helper(m_uri, m_resource.get_location(), disk_path, "File permission denied", 403);
+        return forbidden();
     }
-    router_log_helper(resource, location, sys_path, "Will be served", 200);
-    return RouterResult(200, sys_path, RouterResult::FILE_PATH);
+    router_log_helper(m_uri, m_resource.get_location(), disk_path, "Will be served", 200);
+    return RouterResult(200, disk_path, RouterResult::FILE_PATH);
 }
-
-// RouterResult router_get_resource_delete(const s_Server &server, const std::string &resource)
-// {
-//     const s_Location *location  = router_get_best_matched_location(server, resource);
-//     struct stat       file_info = {};
-//
-//     // TODO: Handle request type GET/HEAD...
-//
-//     if (!location)
-//     {
-//         router_log_helper(resource, location, "", "no matching location", 404);
-//         return RouterResult(404, get_default_page(server, 404), RouterResult::FILE_PATH);
-//     }
-//
-//     if (location->redirect_code != 0)
-//     {
-//         std::string redirect_uri(resource);
-//         redirect_uri.erase(0, location->path.size());
-//         redirect_uri = join_path(location->redirect_path, redirect_uri);
-//         if (redirect_uri[0] != '/')
-//         {
-//             redirect_uri = "/" + redirect_uri;
-//         }
-//         router_log_helper(resource, location, redirect_uri, "Redirection location", location->redirect_code);
-//         return RouterResult(location->redirect_code, redirect_uri, RouterResult::REDIRECTION);
-//     }
-//
-//     if (location->root.empty())
-//     {
-//         router_log_helper(resource, location, "", "Root directory is not configured", 404);
-//         return RouterResult(404, get_default_page(server, 404), RouterResult::FILE_PATH);
-//     }
-//
-//     std::string sys_path(resource);
-//     sys_path.erase(0, location->path.size());
-//
-//     if (!path_traverse_is_safe(sys_path)) // Prevent path traversing like: ../././../..
-//     {
-//         router_log_helper(resource, location, sys_path, "Attempting to go above root directory", 400);
-//         return RouterResult(400, get_default_page(server, 400), RouterResult::FILE_PATH);
-//     }
-//
-//     sys_path = join_path(location->root, sys_path);
-//
-//     if (stat(sys_path.c_str(), &file_info) != 0)
-//     {
-//         router_log_helper(resource, location, sys_path, "File or directory does not exist", 404);
-//         return RouterResult(404, get_default_page(server, 404), RouterResult::FILE_PATH);
-//     }
-//     if (S_ISDIR(file_info.st_mode))
-//     {
-//         if (resource.empty() || resource[resource.size() - 1] != '/')
-//         {
-//             router_log_helper(resource, location, resource + "/", "Directory; deleting without trailing slash", 409);
-//             return RouterResult(409, resource + "/", RouterResult::FILE_PATH);
-//         }
-//         else if (try_unlink(sys_path))
-//         {
-//             // TODO: if cannot be deleted respond with 403
-//             router_log_helper(resource, location, sys_path, "delete Permission denied", 403);
-//             return RouterResult(403, get_default_page(server, 403), RouterResult::FILE_PATH);
-//         }
-//         else
-//         {
-//             router_log_helper(resource, location, sys_path, "Deleting directory", 204);
-//             return RouterResult(204, get_default_page(server, 204), RouterResult::FILE_PATH);
-//         }
-//     }
-//
-//     if (!try_unlink(sys_path))
-//     {
-//         router_log_helper(resource, location, sys_path, "unlink(): File permission denied", 403);
-//         return RouterResult(403, get_default_page(server, 403), RouterResult::FILE_PATH);
-//     }
-//     router_log_helper(resource, location, sys_path, "unlink()", 204);
-//     return RouterResult(204, sys_path, RouterResult::FILE_PATH);
-// }
---
+++ src/Router/RouterResource.cpp
@@ -0,0 +1,265 @@
+#include "RouterResource.hpp"
+
+/**
+ * Simple logging utility for debugging, sometime in the near future you'll ask what WTF that just happend this will
+ * help!
+ */
+void router_log_helper(const std::string &resource, const s_Location *loc, const std::string &file_path,
+                       const std::string &msg, int http_code)
+{
+    LOG_DEBUG("ROUTER") << "[URI=" << resource << "]";
+    if (loc)
+        std::cout << " [config_loc=" << loc->path << "]";
+    std::cout << " [";
+    if (!file_path.empty())
+        std::cout << file_path;
+    std::cout << ": " << msg << "]" << " (status_code=" << http_code << ")\n";
+}
+
+RouterResource::RouterResource(const s_Server &server, const std::string &uri, const std::string &method) :
+    m_location(NULL),
+    m_uri(uri),
+    m_method(method),
+    m_file_info(),
+    m_exists(false),
+    m_is_directory(false),
+    m_readable(false),
+    m_router_result(404, get_default_page(server, 404), RouterResult::FILE_PATH),
+    m_has_early_response(false)
+{
+    m_location = get_best_matched_location(server, m_uri);
+
+    if (!m_location)
+    {
+        router_log_helper(m_uri, m_location, "", "no matching location", 404);
+        set_early_response(404, server);
+        return;
+    }
+
+    if (m_location->redirect_code != 0)
+    {
+        std::string redirect_uri(m_uri);
+        redirect_uri.erase(0, m_location->path.size());
+        redirect_uri = join_path(m_location->redirect_path, redirect_uri);
+        if (redirect_uri[0] != '/')
+        {
+            redirect_uri = "/" + redirect_uri;
+        }
+        router_log_helper(m_uri, m_location, redirect_uri, "Redirection location", m_location->redirect_code);
+        set_early_redirect_response(m_location->redirect_code, redirect_uri);
+        return;
+    }
+
+    if (!location_allowed_method(*m_location, m_method))
+    {
+        router_log_helper(m_uri, m_location, m_method, "Method not allowed", 405);
+        set_early_response(405, server);
+        return;
+    }
+
+    if (m_location->root.empty())
+    {
+        router_log_helper(m_uri, m_location, "", "Root directory is not configured", 404);
+        set_early_response(404, server);
+        return;
+    }
+
+    m_disk_path = m_uri;
+    m_disk_path.erase(0, m_location->path.size());
+
+    if (!path_traverse_is_safe(m_disk_path)) // Prevent path traversing like: ../././../..
+    {
+        router_log_helper(m_uri, m_location, m_disk_path, "Attempting to go above root directory", 400);
+        set_early_response(400, server);
+        return;
+    }
+
+    m_disk_path    = join_path(m_location->root, m_disk_path);
+    m_exists       = (stat(m_disk_path.c_str(), &m_file_info) == 0);
+    m_is_directory = m_exists && S_ISDIR(m_file_info.st_mode);
+    m_readable     = access(m_disk_path.c_str(), R_OK) == 0;
+}
+
+const s_Location *RouterResource::get_best_matched_location(const s_Server &server, const std::string &resource)
+{
+    const s_Location *location = NULL;
+
+    for (size_t j = 0; j < server.locations.size(); ++j)
+    {
+        if (location_matcher(resource, server.locations[j].path))
+        {
+            if (!location || server.locations[j].path.size() > location->path.size())
+            {
+                location = &server.locations[j];
+            }
+        }
+    }
+    return location;
+}
+
+bool RouterResource::location_matcher(const std::string &resource, const std::string &loc_path)
+{
+    if (!string_starts_with(resource, loc_path))
+    {
+        return false;
+    }
+    if (resource.size() == loc_path.size())
+    {
+        return true;
+    }
+    if (loc_path[loc_path.size() - 1] == '/')
+    {
+        return true;
+    }
+    return resource[loc_path.size()] == '/';
+}
+
+void RouterResource::set_early_response(int http_code, const s_Server &server)
+{
+    m_has_early_response = true;
+    m_router_result      = RouterResult(http_code, get_default_page(server, http_code), RouterResult::FILE_PATH);
+}
+
+void RouterResource::set_early_redirect_response(int http_code, const std::string &location)
+{
+    m_has_early_response = true;
+    m_router_result      = RouterResult(http_code, location, RouterResult::REDIRECTION);
+}
+
+std::string RouterResource::get_default_page(const s_Server &server, int status_code)
+{
+    std::map<int, std::string>::const_iterator it = server.error_page.find(status_code);
+    if (it == server.error_page.end())
+        return "";
+    std::string page_path = it->second;
+    if (is_file_regular(page_path))
+    {
+        return page_path;
+    }
+    return "";
+}
+
+std::string RouterResource::join_path(const std::string &root, const std::string &rest)
+{
+    if (root.empty() || rest.empty())
+    {
+        return root + rest;
+    }
+
+    bool rootSlash = root[root.size() - 1] == '/';
+    bool restSlash = rest[0] == '/';
+    if (rootSlash && restSlash)
+    {
+        return root + rest.substr(1);
+    }
+    if (!rootSlash && !restSlash)
+    {
+        return root + "/" + rest;
+    }
+    return root + rest;
+}
+
+bool RouterResource::location_allowed_method(const s_Location &location, const std::string &method)
+{
+    const std::vector<std::string> &methods = location.methods;
+
+    return method == "GET" || std::find(methods.begin(), methods.end(), method) != methods.end();
+}
+
+static bool cstr_cmp_by_size(const char *s1, const char *s2, std::size_t size)
+{
+    for (std::size_t i = 0; i < size; i++)
+    {
+        if (s1[i] != s2[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool RouterResource::path_traverse_is_safe(const std::string &str)
+{
+    const char       *path  = str.data();
+    const std::size_t len   = str.size();
+    int               level = 0;
+    size_t            i     = 0;
+
+    while (i < len)
+    {
+        while (i < len && path[i] == '/')
+        {
+            i++;
+        }
+        if (i >= len)
+        {
+            break;
+        }
+
+        std::size_t seg_len = 0;
+        while (i < len && path[i] != '/')
+        {
+            i++;
+            seg_len++;
+        }
+
+        // TODO: Can "...." and "..."  cause a problem?
+        if (seg_len == 2 && cstr_cmp_by_size(&path[i - seg_len], "..", 2))
+        {
+            level--;
+            if (level < 0)
+            {
+                return false;
+            }
+        }
+        else if (seg_len == 1 && path[i - seg_len] == '.')
+        {
+            continue;
+        }
+        else if (seg_len > 0)
+        {
+            level++;
+        }
+    }
+
+    return level >= 0;
+}
+
+bool RouterResource::has_early_response()
+{
+    return m_has_early_response;
+}
+
+RouterResult RouterResource::get_early_router_result()
+{
+    if (!m_has_early_response)
+    {
+        LOG_ERROR("RouterResource") << "[uri=" << m_uri << "] Attempting to get none existing early router result!\n";
+    }
+    return m_router_result;
+}
+
+std::string RouterResource::get_disk_path()
+{
+    return m_disk_path;
+}
+
+bool RouterResource::exists()
+{
+    return m_exists;
+}
+
+bool RouterResource::is_directory()
+{
+    return m_is_directory;
+}
+
+bool RouterResource::is_readable()
+{
+    return m_readable;
+}
+
+const s_Location *RouterResource::get_location()
+{
+    return m_location;
+}
