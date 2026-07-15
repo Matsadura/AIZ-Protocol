b41e4d51a042f14ed0134baadf37120b226a1311
ila36ix <jbariali002@gmail.com>
2026-07-15 06:43:42 +0100

    [UPDATE] Add RouterResource to use in all methods

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -13,4 +13,24 @@ int                      hexToInt(char c);
 std::string              toLower(const std::string &str);
 bool                     isNumeric(const std::string &str);
 
+/**
+ * Return: true if @path is a regular file
+ */
+bool is_file_regular(const std::string &path);
+
+/**
+ * Return: true if @filepath is can be read
+ */
+bool is_file_readable(const std::string &filepath);
+
+/**
+ * Return: true if @filepath is can be executed
+ */
+bool is_file_executable(const std::string &filepath);
+
+/**
+ * Returns a Boolean stating whether a string starts with the specified prefix
+ */
+bool string_starts_with(const std::string &bigger_string, const std::string &prefix);
+
 #endif /* UTILS_HPP */
