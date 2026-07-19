1c23c7b8a3bb6032eb314a513387acd56f0add69
Matsadura <zidanditoo1@gmail.com>
2026-07-19 12:46:40 +0100

    [UPDATE] Use vector<char> body and add consume mechanism for 0 allocation streaming

--- src/CGI/CGIResponse.cpp
+++ src/CGI/CGIResponse.cpp
@@ -1,4 +1,5 @@
 #include "CGIResponse.hpp"
+#include <cstddef>
 #include <sstream>
 
 CGIResponse::CGIResponse() : m_cgi_state(CGI_IDLE), m_is_local_redirect(false), m_error_code(0)
@@ -30,7 +31,7 @@ int CGIResponse::getErrorCode() const
 }
 
 /**
- * appendCgiData - Appends CGI data to the internal buffer.
+ * appendCgiData - Appends CGI data and formats it immediately into HTTP chunks.
  * @data: A pointer to the data to be appended.
  * @length: The length of the data to be appended.
  */
@@ -38,8 +39,10 @@ void CGIResponse::appendCgiData(const char *data, size_t length)
 {
     if (m_error_code != 0)
         return;
+
     if (m_cgi_state == CGI_IDLE)
         m_cgi_state = CGI_READING_HEADERS;
+
     if (m_cgi_state == CGI_READING_HEADERS)
     {
         m_cgi_header_buffer.append(data, length);
@@ -47,14 +50,26 @@ void CGIResponse::appendCgiData(const char *data, size_t length)
             m_cgi_state = CGI_STREAMING_BODY;
     }
     else if (m_cgi_state == CGI_STREAMING_BODY)
-        m_body_buffer.insert(m_body_buffer.end(), data, data + length);
+    {
+        if (length > 0)
+        {
+            std::ostringstream hex_size;
+            hex_size << std::hex << length << "\r\n";
+            std::string chunk_hdr = hex_size.str();
+
+            m_body_buffer.insert(m_body_buffer.end(), chunk_hdr.begin(), chunk_hdr.end());
+            m_body_buffer.insert(m_body_buffer.end(), data, data + length);
+            m_body_buffer.push_back('\r');
+            m_body_buffer.push_back('\n');
+        }
+    }
 }
 
 /**
- * translateToHttp - Translates CGI headers to HTTP response headers.
+ * translateToHttp - Translates CGI headers to an HTTP response string.
  * @cgi_headers: A map containing CGI headers to be translated.
  */
-void CGIResponse::translateToHttp(std::map<std::string, std::string> &cgi_headers)
+std::string CGIResponse::translateToHttp(std::map<std::string, std::string> &cgi_headers)
 {
     std::ostringstream http_headers;
     std::string        status = "200 OK";
@@ -87,12 +102,11 @@ void CGIResponse::translateToHttp(std::map<std::string, std::string> &cgi_header
     }
 
     http_headers << "\r\n";
-    m_http_response_headers = http_headers.str();
+    return http_headers.str();
 }
 
 /**
- * parseCgiHeaders - Parses the CGI headers from the buffer.
- * Return: true if headers are complete and valid, false if more data is needed or if there's an error.
+ * parseCgiHeaders - Parses the CGI headers and injects HTTP framing into the output buffer.
  */
 bool CGIResponse::parseCgiHeaders()
 {
@@ -111,8 +125,6 @@ bool CGIResponse::parseCgiHeaders()
     std::string headers_part  = m_cgi_header_buffer.substr(0, boundary_pos);
     std::string leftover_body = m_cgi_header_buffer.substr(boundary_pos + boundary_len);
 
-    m_body_buffer.insert(m_body_buffer.end(), leftover_body.begin(), leftover_body.end());
-
     std::vector<std::string>           header_lines = split(headers_part, '\n');
     std::map<std::string, std::string> parsed_cgi_headers;
 
@@ -120,58 +132,52 @@ bool CGIResponse::parseCgiHeaders()
     {
         std::string line = *it;
 
-        // Remove trailing \r if it exists (since we split by \n)
         if (!line.empty() && line[line.length() - 1] == '\r')
             line.erase(line.length() - 1);
 
         if (line.empty())
             continue;
 
-        // RULE: CGI/1.1 does not support continuation lines
         if (line[0] == ' ' || line[0] == '\t')
         {
-            m_error_code = 502;
+            generateErrorResponse(502);
             return true;
         }
 
         size_t colon_pos = line.find(':');
         if (colon_pos == std::string::npos)
         {
-            m_error_code = 502;
+            generateErrorResponse(502);
             return true;
         }
 
         std::string key   = line.substr(0, colon_pos);
         std::string value = line.substr(colon_pos + 1);
 
-        // RULE: No whitespace between field name and colon
         if (!key.empty() && (key[key.length() - 1] == ' ' || key[key.length() - 1] == '\t'))
         {
-            m_error_code = 502;
+            generateErrorResponse(502);
             return true;
         }
 
         trim(key);
         trim(value);
-        // RULE: case insensitive field names, but we will store them in lowercase for consistency
         std::transform(key.begin(), key.end(), key.begin(), ::tolower);
 
         if (!isValidHeaderName(key) || !isValidHeaderValue(value))
         {
-            m_error_code = 502;
+            generateErrorResponse(502);
             return true;
         }
 
-        // RULE: NULL field value is equivalent to a field not being sent
         if (value.empty())
             continue;
 
-        // RULE: CGI fields MUST NOT appear more than once
         if (key == "content-type" || key == "location" || key == "status")
         {
             if (parsed_cgi_headers.count(key) > 0)
             {
-                m_error_code = 502;
+                generateErrorResponse(502);
                 return true;
             }
         }
@@ -179,76 +185,76 @@ bool CGIResponse::parseCgiHeaders()
         parsed_cgi_headers[key] = value;
     }
 
-    // RULE: At least one CGI field MUST be supplied
     if (parsed_cgi_headers.empty())
     {
-        m_error_code = 502;
+        generateErrorResponse(502);
         return true;
     }
 
-    translateToHttp(parsed_cgi_headers);
+    std::string http_headers = translateToHttp(parsed_cgi_headers);
+    m_body_buffer.insert(m_body_buffer.end(), http_headers.begin(), http_headers.end());
+
+    if (!leftover_body.empty())
+    {
+        std::ostringstream hex_size;
+        hex_size << std::hex << leftover_body.size() << "\r\n";
+        std::string chunk_hdr = hex_size.str();
+
+        m_body_buffer.insert(m_body_buffer.end(), chunk_hdr.begin(), chunk_hdr.end());
+        m_body_buffer.insert(m_body_buffer.end(), leftover_body.begin(), leftover_body.end());
+        m_body_buffer.push_back('\r');
+        m_body_buffer.push_back('\n');
+    }
+
     return true;
 }
 
 /**
- * getOutputChunk - Retrieves the next chunk of output to be sent to the client.
- * Return: A string containing the next chunk of output, or an empty string if there's no more data.
+ * appendTerminalChunk - Pushes the final EOF chunk into the output queue.
  */
-std::string CGIResponse::getOutputChunk()
-{
-    if (m_error_code != 0)
-        return "";
-
-    std::string output;
-
-    if (!m_http_response_headers.empty())
-    {
-        output += m_http_response_headers;
-        m_http_response_headers.clear();
-    }
-
-    if (!m_body_buffer.empty())
-    {
-        std::ostringstream hex_size;
-        hex_size << std::hex << m_body_buffer.size() << "\r\n";
-
-        output += hex_size.str();
-        output.append(m_body_buffer.begin(), m_body_buffer.end());
-        output += "\r\n";
-
-        m_body_buffer.clear();
-    }
-
-    return output;
-}
-
-/**
- * getTerminalChunk - Retrieves the terminal chunk indicating the end of the response.
- * Return: A string containing the terminal chunk, or an empty string if not applicable.
- */
-std::string CGIResponse::getTerminalChunk()
+void CGIResponse::appendTerminalChunk()
 {
     if (m_cgi_state == CGI_STREAMING_BODY && m_error_code == 0)
     {
-        m_cgi_state = CGI_COMPLETE;
-        return "0\r\n\r\n";
+        m_cgi_state      = CGI_COMPLETE;
+        std::string term = "0\r\n\r\n";
+        m_body_buffer.insert(m_body_buffer.end(), term.begin(), term.end());
     }
-    return "";
 }
 
 /**
- * generateErrorResponse - Generates a simple HTTP error response for the given error code.
+ * generateErrorResponse - Clears partial output and injects an HTTP error response into the buffer.
  * @error_code: The HTTP error code to include in the response.
- * Return: A string containing the HTTP error response.
  */
-std::string CGIResponse::generateErrorResponse(int error_code)
+void CGIResponse::generateErrorResponse(int error_code)
 {
     if (error_code == 0)
-        return "";
+        return;
+
+    m_body_buffer.clear();
+
     std::ostringstream response;
     response << "HTTP/1.1 " << error_code << " Error\r\n";
     response << "Content-Type: text/plain\r\n";
     response << "Content-Length: 0\r\n";
     response << "\r\n";
-    return response.str();
+
+    std::string err_str = response.str();
+
+    m_body_buffer.insert(m_body_buffer.end(), err_str.begin(), err_str.end());
+
+    m_cgi_state  = CGI_COMPLETE;
+    m_error_code = error_code;
+}
+
+void CGIResponse::consumeBodyChunk(size_t length)
+{
+    if (length >= m_body_buffer.size())
+    {
+        m_body_buffer.clear();
+    }
+    else
+    {
+        m_body_buffer.erase(m_body_buffer.begin(), m_body_buffer.begin() + static_cast<long>(length));
+    }
 }
--- src/CGI/CGIResponse.hpp
+++ src/CGI/CGIResponse.hpp
@@ -15,19 +15,21 @@ class CGIResponse
         CGI_COMPLETE
     };
 
-    void        appendCgiData(const char *data, size_t length);
-    bool        parseCgiHeaders();
-    std::string getOutputChunk();
-    std::string getTerminalChunk();
+    void              appendCgiData(const char *data, size_t length);
+    bool              parseCgiHeaders();
+    void              appendTerminalChunk();
+    std::vector<char> getBodyBuffer();
 
-    void        setCgiState(CgiState state);
-    CgiState    getCgiState() const;
-    int         getErrorCode() const;
-    std::string generateErrorResponse(int error_code);
+    void     setCgiState(CgiState state);
+    CgiState getCgiState() const;
+    int      getErrorCode() const;
+    void     generateErrorResponse(int error_code);
+
+    void consumeBodyChunk(size_t Length);
 
     bool isLocalRedirect() const;
 
-    void translateToHttp(std::map<std::string, std::string> &cgi_headers);
+    std::string translateToHttp(std::map<std::string, std::string> &cgi_headers);
 
     CGIResponse();
     ~CGIResponse();
@@ -35,7 +37,6 @@ class CGIResponse
   private:
     CgiState          m_cgi_state;
     std::string       m_cgi_header_buffer;
-    std::string       m_http_response_headers;
     std::vector<char> m_body_buffer;
     bool              m_is_local_redirect;
     int               m_error_code;
