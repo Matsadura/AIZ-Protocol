63dd6018931ab8bb016b8c03936d2046f2a8efab
ila36ix <jbariali002@gmail.com>
2026-08-06 23:17:01 +0100

    [ADD] The main() entry point

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,14 +1,20 @@
 #include "../src/core/Multiplexer.h"
 
-int main()
+int main(int ac, char **av)
 {
+    if (ac != 2)
+    {
+        std::cerr << "Usage: " << av[0] << " <file.conf>\n";
+        return 1;
+    }
+
     try
     {
-        Multiplexer server("configfile/aiz.conf");
+        Multiplexer server(av[1]);
         server.run();
     }
     catch (std::exception &e)
     {
-        std::cout << e.what() << "\n";
+        std::cout << "ERROR: " << e.what() << "\n";
     }
 }
